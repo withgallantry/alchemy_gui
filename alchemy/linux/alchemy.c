0df1824a8cdbcc50f1e1ddda364728d00bc479a2
Matty Gallantry <matt@wakelet.com>
2020-02-19 21:35:00 +0000

    Updated interface

--- alchemy/linux/alchemy.c
+++ alchemy/linux/alchemy.c
@@ -18,9 +18,6 @@
 
 // Include any extended elements
 //<Includes !Start!>
-// Include extended elements
-#include "elem/XSlider.h"
-#include "elem/XTextbox.h"
 //<Includes !End!>
 
 // ------------------------------------------------
@@ -35,10 +32,9 @@
 // ------------------------------------------------
 //<Fonts !Start!>
 #define FONT_DROID_MONO8 "/usr/share/fonts/truetype/droid/DroidSansMono.ttf"
-#define FONT_DROID_MONO8 "/usr/share/fonts/truetype/droid/DroidSansMono.ttf"
-#define FONT_DROID_MONO18 "/usr/share/fonts/truetype/droid/DroidSansMono.ttf"
 //<Fonts !End!>
 
+bool  m_bQuit = false
 
 // ------------------------------------------------
 // Defines for resources
@@ -51,12 +47,10 @@
 // ------------------------------------------------
 //<Enum !Start!>
 enum {E_PG_MAIN};
-enum {EN_JOYSTICK_BTN,E_ELEM_TEXT1,E_ELEM_TEXTBOX1,E_TXTSCROLL1};
+enum {EN_JOYSTICK_BTN,E_ELEM_BTN3,E_ELEM_BTN4,E_ELEM_BTN6};
 // Must use separate enum for fonts with MAX_FONT at end to use gslc_FontSet.
-enum {E_DROID_MONO18,E_DROID_MONO8,MAX_FONT};
+enum {E_DROID_MONO8,MAX_FONT};
 //<Enum !End!>
-
-bool        m_bQuit = false;
 
 // ------------------------------------------------
 // Instantiate the GUI
@@ -68,7 +62,7 @@ bool        m_bQuit = false;
 //<ElementDefines !Start!>
 #define MAX_PAGE                1
 
-#define MAX_ELEM_PG_MAIN 5                                          // # Elems total on page
+#define MAX_ELEM_PG_MAIN 4                                          // # Elems total on page
 #define MAX_ELEM_PG_MAIN_RAM MAX_ELEM_PG_MAIN // # Elems in RAM
 //<ElementDefines !End!>
 
@@ -84,9 +78,6 @@ gslc_tsPage                     m_asPage[MAX_PAGE];
 //<GUI_Extra_Elements !Start!>
 gslc_tsElem                     m_asPage1Elem[MAX_ELEM_PG_MAIN_RAM];
 gslc_tsElemRef                  m_asPage1ElemRef[MAX_ELEM_PG_MAIN];
-gslc_tsXTextbox                 m_sTextbox1;
-char                            m_acTextboxBuf1[168]; // NRows=6 NCols=28
-gslc_tsXSlider                  m_sTextScroll1;
 
 #define MAX_STR                 100
 
@@ -98,8 +89,6 @@ gslc_tsXSlider                  m_sTextScroll1;
 
 // Save some element references for direct access
 //<Save_References !Start!>
-gslc_tsElemRef*  m_pElemTextbox1   = NULL;
-gslc_tsElemRef*  m_pTextSlider1    = NULL;
 //<Save_References !End!>
 
 
@@ -148,6 +137,15 @@ bool CbBtnCommon(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int1
         //TODO- Replace with button handling code
         break;
 
+      case E_ELEM_BTN3:
+        //TODO- Replace with button handling code
+        break;
+      case E_ELEM_BTN4:
+        //TODO- Replace with button handling code
+        break;
+      case E_ELEM_BTN6:
+        //TODO- Replace with button handling code
+        break;
 //<Button Enums !End!>
       default:
         break;
@@ -177,11 +175,6 @@ bool CbSlidePos(void* pvGui,void* pvElemRef,int16_t nPos)
   // From the element's ID we can determine which slider was updated.
   switch (pElem->nId) {
 //<Slider Enums !Start!>
-    case E_TXTSCROLL1:
-      // Fetch the slider position
-      nVal = gslc_ElemXSliderGetPos(pGui,m_pTextSlider1);
-      //TODO- Replace with slider handling code
-      break;
 
 //<Slider Enums !End!>
     default:
@@ -207,43 +200,40 @@ bool InitGUI(char *strPath)
   //       ensure that the main page is the correct page no matter the add order.
   gslc_SetPageCur(&m_gui,E_PG_MAIN);
 
-  
+
   // Set Background to a flat color
   gslc_SetBkgndColor(&m_gui,GSLC_COL_BLACK);
 
   // -----------------------------------
   // PAGE: E_PG_MAIN
-  
-  // Create E_ELEM_TEXT1 text label
-  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT1,E_PG_MAIN,(gslc_tsRect){187,31,286,43},
-    (char*)"Alchemy Stone",0,E_DROID_MONO18);
-  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
-  
+
   // create EN_JOYSTICK_BTN button with text label
   pElemRef = gslc_ElemCreateBtnTxt(&m_gui,EN_JOYSTICK_BTN,E_PG_MAIN,
-    (gslc_tsRect){220,115,200,40},(char*)"Enable Joystick",0,E_DROID_MONO8,&CbBtnCommon);
+    (gslc_tsRect){210,115,220,40},(char*)"Enable Joystick",0,E_DROID_MONO8,&CbBtnCommon);
   gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
   gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_BLUE_DK2,GSLC_COL_TEAL,GSLC_COL_GREEN_DK4);
-   
-  // Create wrapping box for textbox E_ELEM_TEXTBOX1 and scrollbar
-  pElemRef = gslc_ElemCreateBox(&m_gui,GSLC_ID_AUTO,E_PG_MAIN,(gslc_tsRect){25,233,201,63});
-  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_GRAY,GSLC_COL_BLACK,GSLC_COL_BLACK);
-  
-  // Create textbox
-  pElemRef = gslc_ElemXTextboxCreate(&m_gui,E_ELEM_TEXTBOX1,E_PG_MAIN,&m_sTextbox1,
-    (gslc_tsRect){25+2,233+4,201-23,63-7},E_DROID_MONO8,
-    (char*)&m_acTextboxBuf1,6,28);
-  gslc_ElemXTextboxWrapSet(&m_gui,pElemRef,true);
-  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_YELLOW);
-  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_GRAY,GSLC_COL_BLACK,GSLC_COL_BLACK);
-  m_pElemTextbox1 = pElemRef;
+  gslc_ElemSetRoundEn(&m_gui, pElemRef, true);
 
-  // Create vertical scrollbar for textbox
-  pElemRef = gslc_ElemXSliderCreate(&m_gui,E_TXTSCROLL1,E_PG_MAIN,&m_sTextScroll1,
-          (gslc_tsRect){25+201-21,233+4,20,63-8},0,100,0,5,true);
-  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_GRAY,GSLC_COL_BLACK,GSLC_COL_BLACK);
-  gslc_ElemXSliderSetPosFunc(&m_gui,pElemRef,&CbSlidePos);
-  m_pTextSlider1 = pElemRef;
+  // create E_ELEM_BTN3 button with text label
+  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN3,E_PG_MAIN,
+    (gslc_tsRect){210,170,220,40},(char*)"Disable WiFi",0,E_DROID_MONO8,&CbBtnCommon);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_BLUE_DK2,GSLC_COL_TEAL,GSLC_COL_GREEN_DK4);
+  gslc_ElemSetRoundEn(&m_gui, pElemRef, true);
+
+  // create E_ELEM_BTN4 button with text label
+  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN4,E_PG_MAIN,
+    (gslc_tsRect){210,225,220,40},(char*)"Enable HUD",0,E_DROID_MONO8,&CbBtnCommon);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_BLUE_DK2,GSLC_COL_TEAL,GSLC_COL_GREEN_DK4);
+  gslc_ElemSetRoundEn(&m_gui, pElemRef, true);
+
+  // create E_ELEM_BTN6 button with text label
+  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN6,E_PG_MAIN,
+    (gslc_tsRect){220,393,200,40},(char*)"Exit",0,E_DROID_MONO8,&CbBtnCommon);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_BLUE_DK2,GSLC_COL_TEAL,GSLC_COL_GREEN_DK4);
+  gslc_ElemSetRoundEn(&m_gui, pElemRef, true);
 //<InitGUI !End!>
 
   return true;
@@ -259,15 +249,13 @@ int main( int argc, char* args[] )
   // ------------------------------------------------
   gslc_InitDebug(&DebugOut);
   UserInitEnv();
-  
+
   if (!gslc_Init(&m_gui,&m_drv,m_asPage,MAX_PAGE,m_asFont,MAX_FONT)) { exit(1); }
 
   // ------------------------------------------------
   // Load Fonts
   // ------------------------------------------------
 //<Load_Fonts !Start!>
-  bOk = gslc_FontSet(&m_gui,E_DROID_MONO18,GSLC_FONTREF_FNAME,FONT_DROID_MONO18,18);
-  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed: %s\n",FONT_DROID_MONO18); exit(1); }
   bOk = gslc_FontSet(&m_gui,E_DROID_MONO8,GSLC_FONTREF_FNAME,FONT_DROID_MONO8,8);
   if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed: %s\n",FONT_DROID_MONO8); exit(1); }
 //<Load_Fonts !End!>
@@ -311,4 +299,3 @@ int main( int argc, char* args[] )
 
   return 0;
 }
-
